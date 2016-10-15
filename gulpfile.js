40e1aa918fe9190a019efa87e9ae713ba239a241
Jeroen Engels <jfm.engels@gmail.com>
2016-10-15 16:55:19 +0200

    Migrate linting from standard to XO (#53)

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,18 @@
-const gulp = require('gulp');
-const del = require('del');
-const babel = require('gulp-babel');
-const eslint = require('gulp-eslint');
-const help = require('gulp-task-listing');
+const gulp = require('gulp')
+const del = require('del')
+const babel = require('gulp-babel')
+const help = require('gulp-task-listing')
 
-gulp.task('help', help);
+gulp.task('help', help)
 
-gulp.task('compile', function () {
+gulp.task('compile', () => {
   return gulp.src('lib/**/*.js')
   .pipe(babel())
-  .pipe(gulp.dest('build/lib'));
-});
+  .pipe(gulp.dest('build/lib'))
+})
 
-gulp.task('lint', function () {
-  return gulp.src([
-    'gulpfile.js',
-    'test/*.js',
-    'lib/**/*.js'
-  ])
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failAfterError());
-});
+gulp.task('clean', () => {
+  return del(['build'])
+})
 
-gulp.task('clean', function () {
-  return del(['build']);
-});
-
-gulp.task('default', ['lint', 'compile']);
+gulp.task('default', ['compile'])
--- test/index.js
+++ test/index.js
@@ -1,21 +1,21 @@
-import test from 'ava';
-import lolex from 'lolex';
-import React from 'react';
-import { render, findDOMNode } from 'react-dom';
-import { Keyframes, Frame } from '../lib';
+import test from 'ava'
+import lolex from 'lolex'
+import React from 'react'
+import {render, findDOMNode} from 'react-dom'
+import {Keyframes, Frame} from '../lib'
 
-let clock;
+let clock
 
-test.before((t) => {
-  clock = lolex.install();
-});
+test.before(() => {
+  clock = lolex.install()
+})
 
-test.after((t) => {
-  clock.uninstall();
-});
+test.after(() => {
+  clock.uninstall()
+})
 
-test('animate', (t) => {
-  const container = document.createElement('div');
+test('animate', t => {
+  const container = document.createElement('div')
   const component = render(
     <Keyframes>
       <Frame duration={100}>foo</Frame>
@@ -23,122 +23,123 @@ test('animate', (t) => {
       <Frame>baz</Frame>
     </Keyframes>,
     container
-  );
-  const node = findDOMNode(component);
+  )
+  const node = findDOMNode(component)
 
-  t.deepEqual(node.childNodes.length, 1);
-  t.deepEqual(node.childNodes[0].textContent, 'foo');
+  t.deepEqual(node.childNodes.length, 1)
+  t.deepEqual(node.childNodes[0].textContent, 'foo')
 
-  clock.tick(100);
+  clock.tick(100)
 
-  t.deepEqual(node.childNodes.length, 1);
-  t.deepEqual(node.childNodes[0].textContent, 'bar');
+  t.deepEqual(node.childNodes.length, 1)
+  t.deepEqual(node.childNodes[0].textContent, 'bar')
 
-  clock.tick(200);
+  clock.tick(200)
 
-  t.deepEqual(node.childNodes.length, 1);
-  t.deepEqual(node.childNodes[0].textContent, 'baz');
-});
+  t.deepEqual(node.childNodes.length, 1)
+  t.deepEqual(node.childNodes[0].textContent, 'baz')
+})
 
-test('Keyframes events', (t) => {
-  const container = document.createElement('div');
+test('Keyframes events', t => {
+  const container = document.createElement('div')
   const onStart = () => {
-    onStart.called = true;
-  };
+    onStart.called = true
+  }
   const onEnd = () => {
-    onEnd.called = true;
-  };
+    onEnd.called = true
+  }
   render(
     <Keyframes onStart={onStart} onEnd={onEnd}>
       <Frame duration={100}>foo</Frame>
       <Frame duration={100}>bar</Frame>
     </Keyframes>,
     container
-  );
+  )
 
-  t.truthy(onStart.called);
-  clock.tick(100);
-  t.falsy(onEnd.called);
-  clock.tick(100);
-  t.truthy(onEnd.called);
-});
+  t.truthy(onStart.called)
+  clock.tick(100)
+  t.falsy(onEnd.called)
+  clock.tick(100)
+  t.truthy(onEnd.called)
+})
 
-test('Frame event', (t) => {
-  const container = document.createElement('div');
+test('Frame event', t => {
+  const container = document.createElement('div')
   const onRender = () => {
-    onRender.called = true;
-  };
+    onRender.called = true
+  }
   render(
     <Keyframes>
       <Frame duration={100}>foo</Frame>
       <Frame duration={100} onRender={onRender}>bar</Frame>
     </Keyframes>,
     container
-  );
+  )
 
-  t.falsy(onRender.called);
-  clock.tick(100);
-  t.truthy(onRender.called);
-});
+  t.falsy(onRender.called)
+  clock.tick(100)
+  t.truthy(onRender.called)
+})
 
-test('set component', (t) => {
-  const container = document.createElement('div');
+test('set component', t => {
+  const container = document.createElement('div')
   const component = render(
+    // eslint-disable-next-line react/forbid-component-props
     <Keyframes component="pre" className="woot">
       <Frame>foo</Frame>
       <Frame>bar</Frame>
     </Keyframes>,
     container
-  );
-  const node = findDOMNode(component);
-  t.deepEqual(node.tagName, 'PRE');
-  t.deepEqual(node.className, 'woot');
-});
+  )
+  const node = findDOMNode(component)
+  t.deepEqual(node.tagName, 'PRE')
+  t.deepEqual(node.className, 'woot')
+})
 
-test('Infinite loop', (t) => {
-  const container = document.createElement('div');
+test('Infinite loop', t => {
+  const container = document.createElement('div')
   const onStart = () => {
-    onStart.called = true;
-  };
+    onStart.called = true
+  }
   const onEnd = () => {
-    onEnd.called = true;
-  };
+    onEnd.called = true
+  }
   render(
     <Keyframes onStart={onStart} onEnd={onEnd} loop>
       <Frame duration={100}>foo</Frame>
       <Frame duration={100}>bar</Frame>
     </Keyframes>,
     container
-  );
+  )
 
-  t.truthy(onStart.called);
-  clock.tick(100);
-  t.falsy(onEnd.called);
-  clock.tick(200);
-  t.falsy(onEnd.called);
-});
+  t.truthy(onStart.called)
+  clock.tick(100)
+  t.falsy(onEnd.called)
+  clock.tick(200)
+  t.falsy(onEnd.called)
+})
 
-test('Finite loop', (t) => {
-  const container = document.createElement('div');
+test('Finite loop', t => {
+  const container = document.createElement('div')
   const onStart = () => {
-    onStart.called = true;
-  };
+    onStart.called = true
+  }
   const onEnd = () => {
-    onEnd.called = true;
-  };
+    onEnd.called = true
+  }
   render(
     <Keyframes onStart={onStart} onEnd={onEnd} loop={3}>
       <Frame duration={100}>foo</Frame>
       <Frame duration={100}>bar</Frame>
     </Keyframes>,
     container
-  );
+  )
 
-  t.truthy(onStart.called);
-  clock.tick(100);
-  t.falsy(onEnd.called);
-  clock.tick(200);
-  t.falsy(onEnd.called);
-  clock.tick(300);
-  t.truthy(onEnd.called);
-});
+  t.truthy(onStart.called)
+  clock.tick(100)
+  t.falsy(onEnd.called)
+  clock.tick(200)
+  t.falsy(onEnd.called)
+  clock.tick(300)
+  t.truthy(onEnd.called)
+})
