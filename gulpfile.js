79e6438948fddf2c249f75c84aa32c579f23f326
Leo Lamprecht <mindrun@icloud.com>
2017-09-18 14:06:49 +0200

    Removed useless package

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 const gulp = require('gulp')
 const del = require('del')
 const babel = require('gulp-babel')
-const help = require('gulp-task-listing')
 
 gulp.task('compile', () => {
   return gulp.src('src/**/*.js')
@@ -10,6 +9,5 @@ gulp.task('compile', () => {
   .pipe(gulp.dest('dist'))
 })
 
-gulp.task('help', help)
 gulp.task('clean', () => del(['dist']))
 gulp.task('default', ['compile'])
