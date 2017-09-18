25470782f1684f1d1de0750181195fa52e5a28c1
Leo Lamprecht <mindrun@icloud.com>
2017-09-18 13:59:06 +0200

    Lowercased text files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
+// Packages
 const gulp = require('gulp')
 const del = require('del')
 const babel = require('gulp-babel')
 const help = require('gulp-task-listing')
-
-gulp.task('help', help)
 
 gulp.task('compile', () => {
   return gulp.src('lib/**/*.js')
@@ -11,8 +10,6 @@ gulp.task('compile', () => {
   .pipe(gulp.dest('build/lib'))
 })
 
-gulp.task('clean', () => {
-  return del(['build'])
-})
-
+gulp.task('help', help)
+gulp.task('clean', () => del(['build']))
 gulp.task('default', ['compile'])
