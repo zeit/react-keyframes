b128564563c29c0ed0a17b0bd21e7bd4b9916f57
Leo Lamprecht <mindrun@icloud.com>
2017-09-18 14:05:47 +0200

    Made build step better

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,11 @@ const babel = require('gulp-babel')
 const help = require('gulp-task-listing')
 
 gulp.task('compile', () => {
-  return gulp.src('lib/**/*.js')
+  return gulp.src('src/**/*.js')
   .pipe(babel())
-  .pipe(gulp.dest('build/lib'))
+  .pipe(gulp.dest('dist'))
 })
 
 gulp.task('help', help)
-gulp.task('clean', () => del(['build']))
+gulp.task('clean', () => del(['dist']))
 gulp.task('default', ['compile'])
